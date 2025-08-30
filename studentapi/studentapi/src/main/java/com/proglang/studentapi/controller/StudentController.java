1380603843820c2633063e7e6867510fb890bb3a
DynnG <geraldyncataluna2nd@gmail.com>
2025-08-30 16:56:08 +0800

    feat: implemented https requests

---
+++ studentapi/studentapi/src/main/java/com/proglang/studentapi/controller/StudentController.java
@@ -0,0 +1,52 @@
+package com.proglang.studentapi.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proglang.studentapi.model.Student;
+import com.proglang.studentapi.service.StudentService;
+
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/students/{id}")
+    public Optional<Student> getStudenBytId(@PathVariable Long id) {
+        return studentService.getStudentByID(id);
+    
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @PutMapping("/students/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        return studentService.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/students/{id}")
+    public Boolean deleteStudent(@PathVariable Long id) {
+        return studentService.deleteStudent(id);
+    }
+}
